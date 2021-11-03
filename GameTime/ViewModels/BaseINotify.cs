aeb77db53640e2c62315561e2d1aeb1cb305d2af
Reza-joon <72915492+Reza-joon@users.noreply.github.com>
2021-11-03 09:26:33 +0100

    viewmodel

--- GameTime/ViewModels/BaseINotify.cs
+++ GameTime/ViewModels/BaseINotify.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
 
 namespace GameTime.ViewModels
 {
-    class BaseINotify
+    /// <summary>
+    /// Base for INotifyPropertyChanged.
+    /// </summary>
+    /// <seealso cref="System.ComponentModel.INotifyPropertyChanged" />
+    public class BaseINotify : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Notifies the property changed.
+        /// </summary>
+        /// <param name="propertyName">Name of the property.</param>
+        protected void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
     }
 }
