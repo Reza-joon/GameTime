cb2f7664fd26d3412c981f75c2a2a0ecef5b7b12
Reza-joon <72915492+Reza-joon@users.noreply.github.com>
2021-11-03 08:55:56 +0100

    first

---
+++ GameTime/Managers/WindowManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+
+namespace GameTime.Managers
+{
+        public class WindowManager
+        {
+
+            public static Window AddGameWindow
+            {
+                get;
+                set;
+            }
+  
+            public static Window ViewGameWindow
+            {
+                get;
+                set;
+            }
+
+            public static Window AddUserWindow
+            {
+                get;
+                set;
+            }
+
+            public static Window ViewUserWindow
+            {
+                get;
+                set;
+            }
+        }
+    }
